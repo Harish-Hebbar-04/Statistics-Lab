5202dad5682ea26b668141f88091b312e6e128d3
Harish-hebbar <132981905+Harish-Hebbar-04@users.noreply.github.com>
2025-07-11 08:24:07 +0530

    Create lab1.R

---
+++ lab1.R
@@ -0,0 +1,13 @@
+data()
+mtcars
+
+cor(mtcars$mpg,mtcars$hp)
+library(ggplot2)
+
+ggplot(data=mtcars,aes(x=hp,y=mpg))+
+         geom_point(shape=4,size=3,color="blue")+
+         geom_smooth(method = "lm" ,se=FALSE)+
+        labs(title="MPG vs HP",
+             x="Horse Power",
+             y="Milage er Galoon")+
+  theme_minimal()
